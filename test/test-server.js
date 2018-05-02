4dc6b51e8e00fd2c9a249f8d352b95a9a467e1f9
Ben Parman <benparman@gmail.com>
2018-05-01 21:42:17 -0600

    Added GET/POST/PUT/DELETE tests for /recipes

--- test/test-server.js
+++ test/test-server.js
@@ -1,7 +1,9 @@
+'use strict';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 const {app, runServer, closeServer} = require('../server');
+
 
 // this lets us use *expect* style syntax in our tests
 // so we can do things like `expect(1 + 1).to.equal(2);`
@@ -136,6 +138,72 @@ describe('Shopping List', function() {
       .then(function(res) {
         return chai.request(app)
           .delete(`/shopping-list/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+      });
+  });
+});
+//----------------------------------------------------------------------------------------
+describe('recipes', function() {
+  before(function() {
+    return runServer();
+  });
+  after(function() {
+    return closeServer();
+  });
+  it('should list recipes on GET', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.at.least(1);
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
+  });
+  it('should add recipes on POST', function() {
+    const newRecipe = {name: 'PB&J', ingredients: ['bread', 'peanut butter', 'jelly']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newRecipe)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('name', 'id', 'ingredients');
+        expect(res.body).not.to.equal(null);
+      });
+  });
+  it('should update recipes on PUT', function() {
+    const updateData = {
+      name: 'fudge',
+      ingredients: ['butter', 'chocolate']
+    };
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+        expect(res.body).to.be.a('object');
+      });
+  });
+  it('should remove items on DELETE', function() {
+    return chai.request(app)
+      .get('/recipes/')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`);
       })
       .then(function(res) {
         expect(res).to.have.status(204);
